6a2eae013676a92fcaf21831dfb7e9d0417cb267
Rustam Sadykov <sadykov.rustam@huawei.com>
2022-09-10 10:44:14 +0300

    fix ServerAPI type

--- ServerAPI/Program.cs
+++ ServerAPI/Program.cs
@@ -1,10 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// TODO: add Database service
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/calculate", () => "Calculate");
-app.MapGet("/register", () => "Register");
-app.MapGet("/signin", () => "Sign in");
+app.MapPost("/register", () => "Register");
+app.MapPost("/signin", () => "Sign in");
 app.MapGet("/history", () => "History");
 
 app.Run();
