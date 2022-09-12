448dcbc547a8ba47018d831d80f7257514882338
bagrorg <54815354+bagrorg@users.noreply.github.com>
2022-09-12 22:53:32 +0300

    Edited domain for calculation history (#1)
    * + calculation data struct
    
    * + fix eof

--- Domain/Calculator.cs
+++ Domain/Calculator.cs
@@ -5,3 +5,5 @@ namespace Domain;
 public readonly record struct CalculationQuery(string QueryString);
 
 public readonly record struct CalculationResult(Result<double> Result);
+
+public readonly record struct CalculationData(CalculationQuery Query, CalculationResult Result);
