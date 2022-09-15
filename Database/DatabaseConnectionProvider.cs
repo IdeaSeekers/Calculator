3788be52d47f1762f8bac5cc247b48d9475d62c9
Daniil Kulikov <kuldanvit@gmail.com>
2022-09-15 14:56:28 +0300

    + ip string in provider

--- Database/DatabaseConnectionProvider.cs
+++ Database/DatabaseConnectionProvider.cs
@@ -4,11 +4,12 @@ namespace Database;
 
 public class DatabaseConnectionProvider
 {
+    private String _serverString = "127.0.0.1";
     private String _connectionString;
 
     public DatabaseConnectionProvider(String databaseUser, String password, String databaseName)
     {
-        _connectionString = "Server=127.0.0.1;" +
+        _connectionString = "Server=" + _serverString + ";" +
                            "User Id=" + databaseUser + ";" +
                            "Password=" + password + ";" +
                            "Database=" + databaseName + ";";
